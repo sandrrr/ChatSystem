e6eff23e856a72b87d74e158bcd5f8ebd3447c30
Onemaeus97 <58248979+Onemaeus97@users.noreply.github.com>
2020-11-23 20:40:28 +0100

    upload User

--- src/Module/User.java
+++ src/Module/User.java
@@ -1,4 +1,64 @@
 package Module;
+import java.net.*;
 //utilisateur
 public class User {
+    private String pseudo;
+    private byte[] adressMAC ;
+    private InetAddress adressIP;
+    
+    User(String pseudo){
+    	//vérification doublante
+    	//notify all users
+        this.pseudo = pseudo;
+        System.out.println("Current pseudo : " + pseudo);
+	    try {
+
+	        this.adressIP = InetAddress.getLocalHost();
+	        System.out.println("Current IP address : " + adressIP.getHostAddress());
+
+	        NetworkInterface network = NetworkInterface.getByInetAddress(adressIP);
+
+	        this.adressMAC = network.getHardwareAddress();
+
+	        System.out.print("Current MAC address : ");
+
+	        StringBuilder sb = new StringBuilder();
+	        for (int i = 0; i < this.adressMAC.length; i++) {
+	            sb.append(String.format("%02X%s", this.adressMAC[i], (i < this.adressMAC.length - 1) ? "-" : ""));        
+	        }
+	        System.out.println(sb.toString());
+
+	    } catch (UnknownHostException e) {
+
+	        e.printStackTrace();
+
+	    } catch (SocketException e){
+
+	        e.printStackTrace();
+
+	    }
+    }
+    public byte[] getAdressMAC() {
+        return adressMAC;
+    }
+
+    public String getPseudo() {
+    	//vérification doublante
+    	//notify all users
+        return pseudo;
+    }
+	public InetAddress getAdressIP() {
+		return adressIP;
+	}
+	public void setAdressIP(InetAddress adressIP) {
+		this.adressIP = adressIP;
+	}
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	public void setAdressMAC(byte[] adressMAC) {
+		this.adressMAC = adressMAC;
+	}
+    
+    
 }
