8a986b28bf19fc0510b180dc85347242316b96c2
Mengxia Shi <shi_mengxia@outlook.com>
2020-11-30 15:16:43 +0100

    Communication Socket

--- src/Module/ChatSession.java
+++ src/Module/ChatSession.java
@@ -1,4 +1,50 @@
 package  Module ;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 //Session pour que deux, voir plusieurs personnes puissent communiquer
-public class ChatSession {
+
+public class ChatSession extends Thread {
+    //class for reading and writing
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+
+    public ChatSession(Socket socket) {
+        System.out.println("Chat session created");
+        try {
+            out = new ObjectOutputStream(socket.getOutputStream());
+            out.flush();
+            in = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.start();
+    }
+
+    public void sendMessage(String text) {
+        try {
+            System.out.println("Send: " + text);
+            out.writeObject(text);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Object objectIn = in.readObject();
+                if (objectIn instanceof String){
+                    System.out.println("Received: " + objectIn);
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
--- src/Module/CommunicationUnicast.java
+++ src/Module/CommunicationUnicast.java
@@ -1,5 +1,33 @@
 package Module;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 //communication de 1 vers 1
 //Principalement pour ChatSession dans le cas de 1 utilisqteur parle à un autre
-public class CommunicationUnicast implements Communication{
+public class CommunicationUnicast extends Thread implements Communication{
+    private ServerSocket serverSocket;
+
+    public CommunicationUnicast(int port){
+        try {
+            serverSocket = new ServerSocket(port);
+            this.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                System.out.println("Socket accepted");
+                new ChatSession(socket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
