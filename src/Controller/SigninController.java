5cf62ce6e2792a41c248b66dd0f4e1bf5c7fad2b
Mengxia Shi <shi_mengxia@outlook.com>
2020-12-10 17:16:54 +0100

    Add style and controller to Signin screen

--- src/Controller/SigninController.java
+++ src/Controller/SigninController.java
@@ -1,4 +1,13 @@
 package Controller;
 
+import javafx.event.Event;
+import javafx.scene.control.TextField;
+
 public class SigninController {
+    public TextField username;
+
+    public void onSubmit(Event e) {
+        String username = this.username.getText();
+        System.out.println(username);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,40 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+    private Stage primaryStage;
+    private Parent root;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/Signin.fxml"));
+        this.primaryStage = primaryStage;
         primaryStage.setTitle("Chat System");
-        primaryStage.setScene(new Scene(root, 1000, 600));
-        primaryStage.show();
+        primaryStage.setAlwaysOnTop(false);
+        primaryStage.setResizable(false);
+        startSignin();
     }
 
+    public void startSignin() {
+        try {
+            root = new FXMLLoader(getClass().getResource("View/Signin.fxml")).load();
+            primaryStage.setScene(new Scene(root, 1000, 600));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startChat() {
+        try {
+            root = new FXMLLoader(getClass().getResource("View/Chat.fxml")).load();
+            primaryStage.setScene(new Scene(root, 1000, 600));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
